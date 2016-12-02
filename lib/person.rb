e6e4ce19765bf08880b50b410cd94173cf935dac
VIC LAM <viclam@VICs-MacBook-Pro.local>
2016-12-02 17:35:43 -0500

    stuffs

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,80 @@
-# your code goes here
+require "pry"
+
+class Person
+  attr_accessor :salary
+  attr_reader :name, :bank_account, :hygiene, :happiness
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def limit_10(item)
+    if item > 10
+      item = 10
+    elsif item < 0
+      item = 0
+    end
+    item
+  end
+
+  def bank_account=(balance)
+    @bank_account = balance
+  end
+
+  def happiness=(new_happiness)
+    @happiness = limit_10(new_happiness)
+  end
+
+  def hygiene=(new_hygiene)
+    @hygiene = self.limit_10(new_hygiene)
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def clean?
+    hygiene > 7
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    if topic == 'politics'
+      self.happiness -= 2
+      friend.happiness -= 2
+      'blah blah partisan blah lobbyist'
+    elsif topic == 'weather'
+      self.happiness += 1
+      friend.happiness += 1
+      'blah blah sun blah rain'
+    else
+      'blah blah blah blah blah'
+    end
+
+  end
+
+end
